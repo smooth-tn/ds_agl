5262c96cdef7d9a2a6974486bedb4a653d4c3aeb
moch-fsegt <mouchir.hassine@etudiant-fsegt.utm.tn>
2026-04-23 23:16:19 +0100

    Added MySQL Script + Squelette DatabaseManager + Squelette MainTest

---
+++ src/dao/DatabaseManager.java
@@ -0,0 +1,35 @@
+package dao;
+
+import java.sql.Connection;
+
+public class DatabaseManager {
+    Connection con = null;
+
+    public DatabaseManager(){
+
+    }
+
+    public void reconnectDB(){
+
+    }
+
+    public void closeDB(){
+
+    }
+
+
+    //  SPLIT THIS LOGIC LATER ON TO SUB DAO LAYERS
+
+    public void chercherEtudiant(int target_id){
+
+    }
+
+    public void chercherEtudiant(String target_username){
+        
+    }
+
+    public void verifierLogin(){
+        
+    }
+
+}
